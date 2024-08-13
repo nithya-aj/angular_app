951f204284cbf7d4fffb073d39ee52fe7ee6cf98
Nithya AJ <aj7nithya@gmail.com>
2024-08-13 16:13:57 +0530

    Create folders and components

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './header.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css', './header.component.css'],
+})
+export class AppComponent {}
